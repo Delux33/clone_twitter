126378e6c92a60bbe0e82924349deaac9d3b30a2
Delux33 <TryaninDM@mail.ru>
2021-11-17 20:57:39 +0300

    Eleventh commit with the sample unit test

---
+++ src/test/java/com/example/delport/LoginTest.java
@@ -0,0 +1,58 @@
+package com.example.delport;
+
+import com.example.delport.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, guest")))
+                .andExpect(content().string(containsString("This is a simple site")));
+    }
+
+    @Test
+    public void accessDeniedTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void correctLoginTest() throws Exception {
+        this.mockMvc.perform(formLogin().user("Admin").password("199"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredentials() throws Exception {
+        this.mockMvc.perform(post("/login").param("user", "Dima"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
+
